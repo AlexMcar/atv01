3496eaca6d98128c010fe0d53654d0a04fdbcf71
AlexMcar <alex19mene@gmail.com>
2021-10-03 21:41:44 -0300

    commit inicial

---
+++ src/aluno/aluno.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function aluno(){
+    return(
+        <View>
+            <Text>
+                Alex Menezes de Carvalho
+            </Text>
+            <Text>
+                21
+            </Text>
+            <Text>
+                Ibiapina
+            </Text>
+            <Text>
+                Design Digital.
+            </Text>
+        </View>
+    )
+}
